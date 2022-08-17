1a632fdeefb9c266337bb0e7029f07eb392cf35d
Soumajit Ghosh <soumojitghosh02@gmail.com>
2022-08-17 20:42:43 +0530

    some changes

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return user_id
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
