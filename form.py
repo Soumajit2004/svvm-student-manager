a72e260435c73cb74559475a539d24c83998e1c4
Soumajit Ghosh <soumojitghosh02@gmail.com>
2022-08-22 10:32:37 +0530

    reformat

--- form.py
+++ form.py
@@ -1,8 +1,21 @@
+import wtforms
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms import StringField, SubmitField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class StudentSearchForm(FlaskForm):
     name = StringField("Student Name", validators=[DataRequired()])
     submit = SubmitField("Search")
+
+
+class StudentEditAddForm(FlaskForm):
+    name = StringField('Student Name', validators=[DataRequired()])
+    grade = IntegerField("Grade", validators=[DataRequired()]),
+    roll_no = IntegerField("Roll No", validators=[DataRequired(), Length(max=4)])
+    father_name = StringField("Father's Name", validators=[DataRequired()])
+    mother_name = StringField("Mother's Name", validators=[DataRequired()])
+    father_mobile_no = IntegerField("Father's Mobile No", validators=[DataRequired(), Length(max=10)])
+    mother_mobile_no = IntegerField("Mother's Mobile No", validators=[DataRequired(), Length(max=10)])
+    address = StringField("Address", validators=[DataRequired(), Length(max=500)]),
+    submit = SubmitField("Save")
